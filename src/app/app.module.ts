0cbed970898fcbbd848354ee2845837cd9e726a9
Tainá Maia <taina.cmaia@hotmail.com>
2022-12-04 18:30:27 -0300

    criação de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './components/button/button.component';
+import { CardComponent } from './components/card/card.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { AchievementComponent } from './components/achievement/achievement.component';
+import { ProfileInfoComponent } from './components/profile-info/profile-info.component';
+import { ResumeComponent } from './components/resume/resume.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonComponent
+    ButtonComponent,
+    CardComponent,
+    ExperienceComponent,
+    EducationComponent,
+    AchievementComponent,
+    ProfileInfoComponent,
+    ResumeComponent,
+    ProfileComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/achievement/achievement.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-achievement',
+  templateUrl: './achievement.component.html',
+  styleUrls: ['./achievement.component.css']
+})
+export class AchievementComponent {
+
+}
