0844a067c3b9e0f34a0a578638bd30904204c372
Mateus Augusto dos Santos Fonseca <mateus.asfonseca@gmail.com>
2022-12-12 16:41:35 -0300

    title da página corrigido

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ProjetoCV';
 
   public data: AppData = {
     profileInfo: {
